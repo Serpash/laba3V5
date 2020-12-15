8fc50765625d9fd81c1327d7480149e1c664c45e
Serpash <ser7290@gmail.com>
2020-12-15 19:31:05 +0300

    ADDED: complete GornerTableCellRender class

--- src/serega/pashko/group7/bsu/GornerTableCellRenderer.java
+++ src/serega/pashko/group7/bsu/GornerTableCellRenderer.java
@@ -11,4 +11,68 @@ import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
 
-public class GornerTableCellRenderer implements TableCellRenderer
+public class GornerTableCellRenderer implements TableCellRenderer{
+
+    private JPanel panel = new JPanel();
+    private JLabel label = new JLabel();
+    // Ищем ячейки, строковое представление которых совпадает с needle
+// (иголкой). Применяется аналогия поиска иголки в стоге сена, в роли
+// стога сена - таблица
+    private String needle = null;
+    private DecimalFormat formatter =
+            (DecimalFormat)NumberFormat.getInstance();
+    public GornerTableCellRenderer() {
+// Показывать только 5 знаков после запятой
+        formatter.setMaximumFractionDigits(5);
+// Не использовать группировку (т.е. не отделять тысячи
+// ни запятыми, ни пробелами), т.е. показывать число как "1000",
+// а не "1 000" или "1,000"
+        formatter.setGroupingUsed(false);
+// Установить в качестве разделителя дробной части точку, а не
+// запятую. По умолчанию, в региональных настройках
+// Россия/Беларусь дробная часть отделяется запятой
+        DecimalFormatSymbols dottedDouble =
+                formatter.getDecimalFormatSymbols();
+        dottedDouble.setDecimalSeparator('.');
+
+        formatter.setDecimalFormatSymbols(dottedDouble);
+// Разместить надпись внутри панели
+        panel.add(label);
+// Установить выравнивание надписи по левому краю панели
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+    }
+    public Component getTableCellRendererComponent(JTable table,
+                                                   Object value, boolean isSelected, boolean hasFocus, int row, int col) {
+// Преобразовать double в строку с помощью форматировщика
+        double temp = (Double)value;
+        String str = String.valueOf(temp);
+        int index = str.lastIndexOf(".");
+        String str2 = str.substring(index+1,str.length());
+        String formattedDouble = formatter.format(value);
+// Установить текст надписи равным строковому представлению числа
+        label.setText(formattedDouble);
+        boolean valid = true;
+        // System.out.println(str);
+
+        char[] a = str2.toCharArray();
+        for(char c:a){
+            valid = (c=='2' || c=='4' || c=='8');
+            if(!valid) break;
+        }
+        if (col==1 && /*needle!=null && *//*needle.equals(formattedDouble)*/ valid) {
+// Номер столбца = 1 (т.е. второй столбец) + иголка не null
+// (значит что-то ищем) +
+// значение иголки совпадает со значением ячейки таблицы -
+// окрасить задний фон панели в красный цвет
+            panel.setBackground(Color.RED);
+        } else {
+// Иначе - в обычный белый
+            panel.setBackground(Color.WHITE);
+        }
+        return panel;
+    }
+    public void setNeedle(String needle) {
+        this.needle = needle;
+    }
+}
+
